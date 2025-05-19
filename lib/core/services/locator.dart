a0fdd4196c1a674ebe3e8985b2e713fdc18db598
yahia samir <yahiasamir70@gmail.com>
2025-05-19 03:57:31 +0300

    Add Hive to Loactor

--- lib/core/services/locator.dart
+++ lib/core/services/locator.dart
@@ -1,9 +1,11 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:news_app/core/netwrok/api_consumer.dart';
 import 'package:news_app/core/netwrok/dio_consumer.dart';
 import 'package:news_app/features/news/data/datasources/local_data_source.dart';
 import 'package:news_app/features/news/data/datasources/remote_data_source.dart';
+import 'package:news_app/features/news/data/models/article_model.dart';
 import 'package:news_app/features/news/data/repositories/news_repository_impl.dart';
 import 'package:news_app/features/news/domain/repositories/news_repository.dart';
 import 'package:news_app/features/news/domain/usecases/get_all_news_usecase.dart';
@@ -11,7 +13,14 @@ import 'package:news_app/features/news/domain/usecases/search_news.dart';
 import 'package:news_app/features/news/presentation/bloc/news_bloc.dart';
 
 final GetIt locator = GetIt.instance;
-void setupLocator() {
+
+Future<void> setupLocator() async {
+  // Initialize Hive first
+  await Hive.initFlutter();
+  Hive.registerAdapter(ArticleModelAdapter());
+  // Open Hive Box
+  await Hive.openBox<ArticleModel>('bookmarks');
+
   // Core
   locator.registerLazySingleton(() => Dio());
 
@@ -19,11 +28,20 @@ void setupLocator() {
   locator.registerLazySingleton<ApiConsumer>(
     () => DioConsumer(client: locator()),
   );
+
   // Data Sources
   locator.registerLazySingleton<RemoteDataSource>(
     () => RemoteDataSource(apiConsumer: locator()),
   );
-  locator.registerLazySingleton<LocalDataSource>(() => LocalDataSource());
+
+  // Local Data Source - use registerSingletonAsync
+  locator.registerSingletonAsync<LocalDataSource>(() async {
+    final box = await Hive.openBox<ArticleModel>('bookmarks');
+    return LocalDataSource(box);
+  });
+
+  // Wait for async registrations to complete
+  await locator.allReady();
 
   // Repository
   locator.registerLazySingleton<NewsRepository>(
@@ -32,10 +50,14 @@ void setupLocator() {
 
   // Use Cases
   locator.registerLazySingleton(() => SearchNewsUseCase(locator()));
-  locator.registerFactory(() => GetAllNewsUseCase(locator()));
+  locator.registerLazySingleton(() => GetAllNewsUseCase(locator()));
 
   // Bloc
   locator.registerFactory(
-    () => NewsBloc(searchNewsUseCase: locator(), getAllNewsUseCase: locator()),
+    () => NewsBloc(
+      searchNewsUseCase: locator(),
+      getAllNewsUseCase: locator(),
+      localDataSource: locator(),
+    ),
   );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:news_app/core/constants/app_constants.dart';
@@ -7,24 +8,16 @@ import 'package:news_app/core/routing/navigator_services.dart';
 import 'package:news_app/core/routing/routes.dart';
 import 'package:news_app/core/services/locator.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:news_app/features/news/data/models/article_model.dart';
+import 'package:news_app/features/news/presentation/bloc/news_bloc.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // Load environment variables
   await dotenv.load();
-  await Hive.initFlutter();
   await EasyLocalization.ensureInitialized();
 
-  // Register Hive Adapter
-  Hive.registerAdapter(ArticleModelAdapter());
-
-  // Open Hive Box
-  await Hive.openBox<ArticleModel>('bookmarks');
-
   // Setup GetIt Service Locator
-  setupLocator();
+  await setupLocator();
 
   runApp(
     EasyLocalization(
@@ -47,15 +40,18 @@ class MainApp extends StatelessWidget {
       minTextAdapt: true,
       splitScreenMode: true,
       builder:
-          (context, child) => MaterialApp(
-            navigatorKey: NavigationService.navigatorKey,
-            debugShowCheckedModeBanner: false,
-            title: AppConstants.appName,
-            locale: context.locale,
-            supportedLocales: context.supportedLocales,
-            localizationsDelegates: context.localizationDelegates,
-            initialRoute: Routes.bottomnav,
-            onGenerateRoute: appRouter.onGenerateRoute,
+          (context, child) => BlocProvider(
+            create: (_) => locator<NewsBloc>()..add(GetAllNews()),
+            child: MaterialApp(
+              navigatorKey: NavigationService.navigatorKey,
+              debugShowCheckedModeBanner: false,
+              title: 'app_title'.tr(),
+              locale: context.locale,
+              supportedLocales: context.supportedLocales,
+              localizationsDelegates: context.localizationDelegates,
+              initialRoute: Routes.bottomnav,
+              onGenerateRoute: appRouter.onGenerateRoute,
+            ),
           ),
     );
   }
