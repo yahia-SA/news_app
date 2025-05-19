01edeaf3b3c391761acf1e4cef683d8df66c7f0c
yahia samir <yahiasamir70@gmail.com>
2025-05-19 03:56:10 +0300

    update Local Data

--- lib/features/news/data/datasources/local_data_source.dart
+++ lib/features/news/data/datasources/local_data_source.dart
@@ -1,15 +1,30 @@
+
 import 'package:hive/hive.dart';
 import 'package:news_app/features/news/data/models/article_model.dart';
 
 class LocalDataSource {
-  final Box<ArticleModel> box = Hive.box<ArticleModel>('bookmarks');
+  final Box<ArticleModel> box;
+
+  LocalDataSource(this.box);
+
+  static Future<LocalDataSource> getInstance() async {
+    if (!Hive.isBoxOpen('bookmarks')) {
+      await Hive.openBox<ArticleModel>('bookmarks');
+    }
+    final box = Hive.box<ArticleModel>('bookmarks');
+    return LocalDataSource(box);
+  }
 
   Future<void> saveArticle(ArticleModel article) async {
     await box.put(article.title, article);
   }
 
-  List<ArticleModel> getSavedArticles() {
+  Future<List<ArticleModel>> getSavedArticles() async {
     return box.values.toList();
+  }
+
+  Future<bool> isArticleBookmarked(String title) async {
+    return box.containsKey(title);
   }
 
   Future<void> deleteArticle(String title) async {
